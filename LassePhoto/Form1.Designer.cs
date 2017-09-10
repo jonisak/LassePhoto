6300cbf06bfab4c4be0707dd5241fce036f898f4
Jonas Isaksson <jonisak@yahoo.com>
2017-09-10 11:06:02 +0200

    å

--- LassePhoto/Form1.Designer.cs
+++ LassePhoto/Form1.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.imgView = new System.Windows.Forms.PictureBox();
+            this.btnOpenImage = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtImageText = new System.Windows.Forms.TextBox();
+            this.lblImageText = new System.Windows.Forms.Label();
+            this.btnSaveImage = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.btnSelectColor = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnShow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgView
+            // 
+            this.imgView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imgView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imgView.Location = new System.Drawing.Point(12, 96);
+            this.imgView.Name = "imgView";
+            this.imgView.Size = new System.Drawing.Size(1028, 513);
+            this.imgView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgView.TabIndex = 0;
+            this.imgView.TabStop = false;
+            this.imgView.Paint += new System.Windows.Forms.PaintEventHandler(this.imgView_Paint_1);
+            // 
+            // btnOpenImage
+            // 
+            this.btnOpenImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenImage.Location = new System.Drawing.Point(12, 43);
+            this.btnOpenImage.Name = "btnOpenImage";
+            this.btnOpenImage.Size = new System.Drawing.Size(279, 47);
+            this.btnOpenImage.TabIndex = 1;
+            this.btnOpenImage.Text = "Öppna bild";
+            this.btnOpenImage.UseVisualStyleBackColor = true;
+            this.btnOpenImage.Click += new System.EventHandler(this.btnOpenImage_Click);
+            // 
+            // txtImageText
+            // 
+            this.txtImageText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtImageText.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtImageText.Location = new System.Drawing.Point(1056, 210);
+            this.txtImageText.Multiline = true;
+            this.txtImageText.Name = "txtImageText";
+            this.txtImageText.Size = new System.Drawing.Size(231, 399);
+            this.txtImageText.TabIndex = 2;
+            this.txtImageText.TextChanged += new System.EventHandler(this.txtImageText_TextChanged);
+            // 
+            // lblImageText
+            // 
+            this.lblImageText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblImageText.AutoSize = true;
+            this.lblImageText.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImageText.Location = new System.Drawing.Point(1053, 185);
+            this.lblImageText.Name = "lblImageText";
+            this.lblImageText.Size = new System.Drawing.Size(46, 22);
+            this.lblImageText.TabIndex = 3;
+            this.lblImageText.Text = "Text";
+            // 
+            // btnSaveImage
+            // 
+            this.btnSaveImage.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnSaveImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveImage.Location = new System.Drawing.Point(297, 43);
+            this.btnSaveImage.Name = "btnSaveImage";
+            this.btnSaveImage.Size = new System.Drawing.Size(279, 47);
+            this.btnSaveImage.TabIndex = 4;
+            this.btnSaveImage.Text = "Spara bild";
+            this.btnSaveImage.UseVisualStyleBackColor = true;
+            this.btnSaveImage.Click += new System.EventHandler(this.btnSaveImage_Click);
+            // 
+            // colorDialog
+            // 
+            this.colorDialog.SolidColorOnly = true;
+            // 
+            // btnSelectColor
+            // 
+            this.btnSelectColor.Location = new System.Drawing.Point(1212, 96);
+            this.btnSelectColor.Name = "btnSelectColor";
+            this.btnSelectColor.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectColor.TabIndex = 5;
+            this.btnSelectColor.UseVisualStyleBackColor = true;
+            this.btnSelectColor.Click += new System.EventHandler(this.btnSelectColor_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(1056, 96);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 32);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.Location = new System.Drawing.Point(602, 43);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(279, 47);
+            this.btnShow.TabIndex = 7;
+            this.btnShow.Text = "Spara bild";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1299, 621);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btnSelectColor);
+            this.Controls.Add(this.btnSaveImage);
+            this.Controls.Add(this.lblImageText);
+            this.Controls.Add(this.txtImageText);
+            this.Controls.Add(this.btnOpenImage);
+            this.Controls.Add(this.imgView);
+            this.Name = "Form1";
+            this.Text = "Lasse Photo";
+            ((System.ComponentModel.ISupportInitialize)(this.imgView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox imgView;
+        private System.Windows.Forms.Button btnOpenImage;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox txtImageText;
+        private System.Windows.Forms.Label lblImageText;
+        private System.Windows.Forms.Button btnSaveImage;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.Button btnSelectColor;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnShow;
     }
 }
 
--- LassePhoto/Form1.cs
+++ LassePhoto/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,173 @@ namespace LassePhoto
         public Form1()
         {
             InitializeComponent();
+
+
+            this.TextSize = 20;
+            numericUpDown1.Value = 20;
         }
+
+
+        Bitmap SelectedImage;
+        Graphics Graphics;
+        Color SelectedColor;
+        int TextSize;
+        
+
+
+        //private bool IsSelecting = false;
+        //// The area we are selecting.
+        //private int X0, Y0, X1, Y1;
+
+        private void btnOpenImage_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
+            int size = -1;
+            DialogResult result = openFileDialog.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog.FileName;
+                try
+                {
+
+
+                    SelectedImage = new Bitmap(file);
+                    //imgView.Image = (Bitmap)SelectedImage.Clone();
+                    imgView.Image = (Bitmap)SelectedImage;
+
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            Console.WriteLine(size); // <-- Shows file size in debugging mode.
+            Console.WriteLine(result); // <-- For debugging use.
+
+
+        }
+
+        private void txtImageText_TextChanged(object sender, EventArgs e)
+        {
+
+            imgView.Refresh();
+            //imgView.Image = null;
+
+
+            //imgView.Image = (Bitmap)SelectedImage.Clone();
+            //Graphics = System.Drawing.Graphics.FromImage(imgView.Image);
+
+            //Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+
+            //var brush = new SolidBrush(Color.FromArgb(255, (byte)this.SelectedColor.R, (byte)this.SelectedColor.G, (byte)this.SelectedColor.B));
+            //Graphics.DrawString(txtImageText.Text, new Font("Tahoma", this.TextSize), brush, new PointF(0, 0));
+
+
+
+            //imgView.Refresh();
+
+        }
+
+        private void btnSaveImage_Click(object sender, EventArgs e)
+        {
+            DialogResult result = saveFileDialog.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = saveFileDialog.FileName;
+                try
+                {
+                    imgView.Image.Save(saveFileDialog.FileName);
+
+
+                    //Jonas
+
+
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            imgView.Image = (Bitmap)SelectedImage.Clone();
+            Graphics = System.Drawing.Graphics.FromImage(imgView.Image);
+
+            Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+
+            var brush = new SolidBrush(Color.FromArgb(255, (byte)this.SelectedColor.R, (byte)this.SelectedColor.G, (byte)this.SelectedColor.B));
+            Graphics.DrawString(txtImageText.Text, new Font("Tahoma", this.TextSize), brush, new PointF(0, 0));
+
+
+            //RectangleF header2Rect = new RectangleF();
+            //using (Font useFont = new Font("Gotham Medium", 28, FontStyle.Bold))
+            //{
+            //    header2Rect.Location = new Point(30, 105);
+            //    header2Rect.Size = new Size(600, ((int)e.Graphics.MeasureString(header2, useFont, 600, StringFormat.GenericTypographic).Height));
+            //    e.Graphics.DrawString(header2, useFont, Brushes.Black, header2Rect);
+            //}
+
+
+
+        }
+
+        private void imgView_Paint_1(object sender, PaintEventArgs e)
+        {
+            //string header2 = "This is a much, much longer Header";
+            //string description = "This is a description of the header.";
+
+            RectangleF header2Rect = new RectangleF();
+            using (Font useFont = new Font("Gotham Medium", 28, FontStyle.Bold))
+            {
+                header2Rect.Location = new Point(30, 105);
+                header2Rect.Size = new Size(600, ((int)e.Graphics.MeasureString(txtImageText.Text, useFont, 600, StringFormat.GenericTypographic).Height));
+                e.Graphics.DrawString(txtImageText.Text, useFont, Brushes.Black, header2Rect);
+            }
+
+            //RectangleF descrRect = new RectangleF();
+            //using (Font useFont = new Font("Gotham Medium", 28, FontStyle.Italic))
+            //{
+            //    descrRect.Location = new Point(30, (int)header2Rect.Bottom);
+            //    descrRect.Size = new Size(600, ((int)e.Graphics.MeasureString(description, useFont, 600, StringFormat.GenericTypographic).Height));
+            //    e.Graphics.DrawString(description.ToLower(), useFont, SystemBrushes.WindowText, descrRect);
+            //}
+        }
+
+        private void imgView_Paint(object sender, PaintEventArgs e)
+        {
+
+            //using (Font myFont = new Font("Arial", 14))
+            //{
+            //    e.Graphics.DrawString("Hello .NET Guide!", myFont, Brushes.Green, new Point(2, 2));
+            //}
+        }
+
+        private void btnSelectColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                btnSelectColor.BackColor = colorDialog.Color;
+                this.SelectedColor = colorDialog.Color;
+
+
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            this.TextSize = (int)numericUpDown1.Value;
+        }
+
+
+
+
+
+   
+
+
+ 
+
     }
 }
